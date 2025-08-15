e4f81ab4950f0fe989d3ece1b449f5eb753eb3b9
Jhonata Samuel <jhonatasammuel@gmail.com>
2025-08-15 17:09:49 -0300

    Load products from the backend

--- data/products.js
+++ data/products.js
@@ -61,6 +61,27 @@ class Clothing extends Product {
 // const date = new Date();
 // console.log(date.toLocaleTimeString());
 
+export let products = [];
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener("load", () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === "clothing") {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    fun();
+  });
+
+  xhr.open("GET", "https://supersimplebackend.dev/products");
+  xhr.send();
+}
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -538,3 +559,4 @@ export const products = [
   }
   return new Product(productDetails);
 });
+*/
